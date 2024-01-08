645311b110dbd7e927788777d7e6bc963402adec
Sumit Rajput <sumitrajput1151999@gmail.com>
2024-01-08 18:20:26 +0530

    prac namaste react

---
+++ src/Components/topheader.js
@@ -0,0 +1,18 @@
+// const Header=()=>{
+//     <div className="header">
+//         <div className="h-image">
+//            <img src="https://pwebassets.paytm.com/commonwebassets/paytmweb/header/images/logo.svg"/>
+//         </div>
+//         <div>Paytm for Consumer</div>
+//         <div>Paytm For Business</div>
+//         <div>Investor Relations</div>
+//         <div>Company</div>
+//         <div>Carrier</div>
+//         <div className="sign-in" >
+//             <div>Sign In</div>
+//             <div className="sign-image">
+//             </div>
+//         </div>
+//     </div>
+// }
+// export default Header;
---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM  from 'react-dom';
+// import Header from './Components/topheader'
+const Applayout=()=>{
+    return (
+        <div>
+              
+        </div>
+    )
+}
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Applayout/>);
